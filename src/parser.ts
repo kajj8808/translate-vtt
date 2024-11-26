746cfa584a9abfaf5464717e4ba4596c220a308a
dlwoqor0820 <dlwoqor0820@naver.com>
2024-11-27 01:01:25 +0900

    ✅parsing 함수 추가(VTT)

--- src/parser.ts
+++ src/parser.ts
@@ -1,5 +1,46 @@
-function test() {
-  console.log(process.env.OPEN_AI_KEY + "pasrser !");
-}
+import fs from "fs";
 
-export default test;
+const FOLDER_PATH = "data/sample/";
+
+const searchFileName = async () => {
+  try {
+    const fileList = await fs.readdirSync(FOLDER_PATH);
+    const fileName = fileList[0];
+    return `${FOLDER_PATH + fileName}`;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const readFile = async (FILE_PATH: string) => {
+  try {
+    const data = await fs.readFileSync(FILE_PATH, "utf-8");
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const dataParsing = (fileData: string) => {
+  let dataObj = [];
+  const dataArray = fileData
+    .split("\n") //줄바꿈으로 리스트 나누기
+    .slice(2) //WEBVVT 헤더 제거
+    .filter((item) => item !== "") //공백 요소 제거
+    .filter((item) => !parseInt(item)); //넘버링 제거
+  for (let i = 0; i < dataArray.length; i++) {
+    if (i % 2 == 0) {
+      dataObj.push({ time: dataArray[i], text: dataArray[i + 1] });
+    }
+  }
+  return dataObj;
+};
+
+const main = async () => {
+  const filePath = await searchFileName();
+  const fileData = await readFile(filePath!);
+  const parsedData = dataParsing(fileData!);
+  return parsedData;
+};
+
+export default main;
