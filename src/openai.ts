a23140e2e3b22aee22ebe323c72f304d96800a1f
kajj8808 <a01039388808@gmail.com>
2024-11-26 23:06:55 +0900

    🏷️ env type 추가

--- src/openai.ts
+++ src/openai.ts
@@ -1,6 +1,8 @@
 import OpenAI from "openai";
 import path from "path";
 
+const API_KEY = process.env.OPEN_AI_KEY;
+
 console.log(path.join("12", "3"));
 
 export function optionTest() {
---
+++ types/process-env.d.ts
@@ -0,0 +1,9 @@
+export {};
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      OPEN_AI_KEY: string;
+    }
+  }
+}
