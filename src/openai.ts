0c77a610008ab39b2f61509223bdc3bc409d4723
kajj8808 <a01039388808@gmail.com>
2024-11-26 23:11:04 +0900

    📦️ langchain추가

--- src/openai.ts
+++ src/openai.ts
@@ -3,12 +3,6 @@ import path from "path";
 
 const API_KEY = process.env.OPEN_AI_KEY;
 
-console.log(path.join("12", "3"));
-
-export function optionTest() {
-  return ["option_1", "option_2"];
-}
-
 function test() {
   return "test text";
 }
