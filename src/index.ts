024f232d4e4f2e4a0db8fad716dd606849338780
kajj8808 <a01039388808@gmail.com>
2024-12-03 21:30:52 +0900

    📦️ ffmpeg사용을 위한 fluent-ffmpeg 추가

--- src/index.ts
+++ src/index.ts
@@ -11,26 +11,7 @@ interface IData {
 import { WebVTTParser } from "webvtt-parser";
 import { formatTime } from "../lib/utile";
 
-(async () => {
-  /*  const vttDatas = await parser();
-  const datas = vttDatas as IData[];
-
-  let results = [] as IData[];
-
-  let count = 0;
-  for (let data of datas) {
-    const tText = await transrateText({ text: data.text, dest: "Korea" });
-    results.push({ text: tText, time: data.time });
-    console.log(datas.length, count++);
-  }
-  const filePath = path.join(
-    DATA_SAMPLE_RESULT_DIR,
-    `test_${new Date().getTime()}.json`
-  );
-  const fileDescriptor = fs.openSync(filePath, "w");
-
-  await fs.writeSync(fileDescriptor, JSON.stringify(results));
-  await fs.closeSync(fileDescriptor); */
+/* (async () => {
   const parser = new WebVTTParser();
   const filePath = path.join(DATA_SAMPLE_DIR, "audio.vtt");
   const vttData = fs.readFileSync(filePath, "utf-8");
@@ -60,4 +41,4 @@ import { formatTime } from "../lib/utile";
     `new_vtt_${new Date().getTime()}.vtt`
   );
   fs.writeFileSync(newPath, newVTT);
-})();
+})(); */
